a1f1bb06fd298e15112e06121af5a68eb84e6cdd
nei.neto <nei.neto@teialabs.com>
2019-04-20 19:25:33 -0300

    Implement cardinality handling for reading n folders from disk

--- data.py
+++ data.py
@@ -1,9 +1,7 @@
 import argparse
-import glob
-import random
-from collections import defaultdict, deque
+from itertools import chain
 from pathlib import Path
-from typing import Callable, Deque, Dict, List, Mapping, Sequence, Tuple, TypeVar, Union
+from typing import Dict, List, Mapping, Sequence, Tuple, Union
 
 import numpy as np
 import torch
@@ -31,11 +29,16 @@ def save_json(
 
 class TieredJPEGs(Dataset):
     '''Images and their JPEG-compressed versions with different levels of quality.'''
+
     def __init__(
         self,
         dataset_root_path: str,
         _transforms: list = None,
         split: str = 'train',
+        tiers: Sequence[str] = ('q40',),  # Order here should be highest to lowest quality, so the model trains on the easier pics first
+        hq_extension: str = 'jpg',  # TODO generalize for several extensions
+        lq_extension: str = 'jpg',
+        flatten: bool = True,  # still not clear how on I'm going to use this...
         debug: bool = False,
     ):
         '''
@@ -45,25 +48,50 @@ class TieredJPEGs(Dataset):
         self.dataset_root = Path(dataset_root_path)
         self.transform = transforms.Compose(_transforms)
         self.split = split
+        self.tiers = tiers
         self.debug = debug
 
-        self.ground_truth_paths: List[str] = sorted((self.dataset_root / 'hq').glob('*.png'))
-        self.q20_paths = sorted((self.dataset_root / 'q20').glob('*.jpg'))
+        self.subfolders: List[Path] = sorted(self.dataset_root.glob(f'{self.split}*'))
 
-    def __getitem__(self, index: int) -> Tuple[Sequence[torch.Tensor], Sequence[torch.Tensor], Sequence[str]]:
-        g_truth = self.ground_truth_paths[index]
-        lq = self.q20_paths[index]
-        assert g_truth.stem == lq.stem
+        self.original_images: List[List[Path]] = [  # shape (num_folders, num_pictures_per_folder)
+            sorted(
+                self.dataset_root.joinpath(subfolder / 'hq').glob(f'*.{hq_extension}')
+            )
+            for subfolder in self.subfolders
+        ]
 
-        good: torch.Tensor = self.transform(Image.open(g_truth).convert('RGB'))
-        bad: torch.Tensor = self.transform(Image.open(lq).convert('RGB'))
+        self.compressed_images: List[List[Path]] = [  # shape (num_subfolders * num_tiers, num_pictures_per_folder)
+            sorted(
+                self.dataset_root.joinpath(subfolder / quality).glob(
+                    f'*.{lq_extension}'
+                )
+            )
+            for subfolder in self.subfolders
+            for quality in self.tiers
+        ]
 
-        return bad, good, lq.stem
+        if flatten:
+            self.original_images = list(chain(*self.original_images))
+            self.compressed_images = list(chain(*self.compressed_images))
+
+    def __getitem__(
+        self, index: int
+    ) -> Tuple[Sequence[torch.Tensor], Sequence[torch.Tensor]]:
+        highq = self.original_images[index]
+        # TODO deal with this. Return all of them? Create different loaders for each? idk
+        lowq = self.compressed_images[index]
+        print(lowq, highq)
+        assert highq.stem == lowq.stem
+
+        hq: torch.Tensor = self.transform(Image.open(highq).convert('RGB'))
+        lq: torch.Tensor = self.transform(Image.open(lowq).convert('RGB'))
+
+        return lq, hq
 
     def __len__(self):
         if self.debug:
             return 2
-        return len(self.ground_truth_paths)
+        return len(self.original_images)
 
     def __repr__(self):
         return f'TieredJPEGs<split={self.split}>'
@@ -76,12 +104,14 @@ transforms_dict = {
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
     ],
     'val': [
-        transforms.FiveCrop((256, 256)),
+        # transforms.FiveCrop((256, 256)),
+        transforms.CenterCrop((256, 256)),
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
     ],
     'test': [
-        transforms.FiveCrop((256, 256)),
+        # transforms.FiveCrop((256, 256)),
+        transforms.CenterCrop((256, 256)),
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
     ],
@@ -93,7 +123,7 @@ def get_loader(
     split: str,
     batch_size: int,
     num_workers: int = 0,
-    debug=False,
+    debug: bool = False,
 ) -> DataLoader:
 
     ds = TieredJPEGs(
@@ -107,14 +137,15 @@ def get_loader(
         dataset=ds,
         batch_size=batch_size,
         shuffle=(split == 'train'),
-        num_workers=num_workers
+        num_workers=num_workers,
     )
 
     return loader
 
 
 if __name__ == '__main__':
-    dataset_root = '/home/nei/projs/hyper-resolution/jpeg-noise-remover/dataset/'
+    # dataset_root = '/home/nei/projs/hyper-resolution/jpeg-noise-remover/dataset/'
+    dataset_root = '/home/nei/projs/hyper-resolution/pytorch-unet/data'
     # import params
     # args = params.get_data_params()
 
@@ -123,4 +154,3 @@ if __name__ == '__main__':
 
     print(stuff, '\n')
     print(stuff[0].shape)
-    print(stuff[2])
